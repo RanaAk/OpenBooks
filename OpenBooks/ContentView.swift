8acb2b161332ecfde1ea7204ca6d3bc30f5c5560
RanaAk <mahediAkbar02@gmail.com>
2024-10-27 17:50:38 +0600

    Initial Commit

---
+++ OpenBooks/ContentView.swift
@@ -0,0 +1,21 @@
+////
+////  ContentView.swift
+////  OpenBooks
+////
+////  Created by RANA  on 26/10/24.
+////
+//
+import SwiftUI
+
+struct ContentView: View {
+   
+    var body: some View {
+        Text("Hello  World")
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ OpenBooks/OpenBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenBooksApp.swift
+//  OpenBooks
+//
+//  Created by RANA  on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
